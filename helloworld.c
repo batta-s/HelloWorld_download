8e9ffad2982b4aaa28c08e0b81b8347238592d50
s-ztn <61276759+s-ztn@users.noreply.github.com>
2020-09-04 12:10:22 +0200

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,14 @@
 int main(){
 
 printf("hello world\n");
+ 
+fd = open("testfile", O_RDWR | O_CREAT);
+for (i=0; i< CONTENT_SIZE; i++)
+{
+    testBufOut[i] = i;
+}
+
+num_bytes_written = write(fd, testBufOut, CONTENT_SIZE);
+close (fd);
 return 0;
 }
